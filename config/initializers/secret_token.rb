8e6722ac799753546a07b4a8f89133b883db7baf
Prelang Builder <builder@prelang.com>
2014-05-29 07:31:58 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WcsSocialNetwork::Application.config.secret_key_base = '0deba6888a1c115d84978e7201a5e753cd877215b5a286390b772cef49fb530dcccdafc682dda5eddc5b8520467727cdc8237f21961d3d2fe43dca59f065c45a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WcsSocialNetwork::Application.config.session_store :cookie_store, key: '_wcs_social_network_session'
